fe7ebe92d53560f248d7959faba8a6eec191544d
Abdallah Askar <57330036+AbdallahAskar1@users.noreply.github.com>
2023-09-19 16:37:26 +0000

    make home page beautiful

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -3,10 +3,19 @@
 import Link from "next/link"
 export default function Home() {
   return (
-    <main>
-      hi
-      <Link href={'/login'}>login</Link>
-      <Link href={'/register'}>Register</Link>
+    <main className="bg-gray-100 min-h-screen flex items-center justify-center">
+     <section className="text-center">
+        <h1 className="text-4xl font-semibold mb-4">Welcome to Your Task Manager</h1>
+        <p className="text-lg text-gray-600 mb-8">Manage your tasks efficiently with time tracking</p>
+        <div className="space-x-4">
+          <Link href="/login"  className="btn bg-blue-500 text-white rounded-full hover:bg-blue-600 py-2  w-[100px]">
+              Login
+          </Link>
+          <Link href="/register" className="btn bg-gray-400  text-white rounded-full hover:bg-gray-600 py-2  w-[100px] ">
+              Register
+          </Link>
+        </div>
+      </section>
       
     </main>
   )
--- frontend/components/api.js
+++ frontend/components/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-export const fetchTasks = () => {
-  return axios.get('http://localhost:3000/tasks');
+export const fetchTasks = (userId) => {
+  return axios.get(`http://localhost:3000/tasks/user/${userId}`);
 };
 
 export const createTask = (task) => {
